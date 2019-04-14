8fd127f132969c164e20af90f31ce5bcacd9a529
El Amraoui Adel <Elamraoui.Adel@hotmail.com>
2019-04-14 20:44:47 +0200

    Server Protocol A begining

---
+++ Project_PC2RServer/sandbox/client/src/prevmain.rs
@@ -0,0 +1,58 @@
+extern crate rand;
+extern crate time;
+use std::io::prelude::*;
+use std::net::TcpStream;
+use std::thread;
+use std::env;
+use rand::prelude::*;
+
+
+
+use time::PreciseTime;
+
+
+fn transform_u64_to_array_of_u8(x: u64) -> [u8; 8] {
+    let b1: u8 = ((x >> 56) & 0xff) as u8;
+    let b2: u8 = ((x >> 48) & 0xff) as u8;
+    let b3: u8 = ((x >> 40) & 0xff) as u8;
+    let b4: u8 = ((x >> 32) & 0xff) as u8;
+    let b5: u8 = ((x >> 24) & 0xff) as u8;
+    let b6: u8 = ((x >> 16) & 0xff) as u8;
+    let b7: u8 = ((x >> 8) & 0xff) as u8;
+    let b8: u8 = (x & 0xff) as u8;
+    return [b1, b2, b3, b4, b5, b6, b7, b8];
+}
+
+fn main() {
+	let args: Vec<String> = env::args().collect();
+	let nbrequest: usize = args[1].parse().unwrap();
+	let start = PreciseTime::now();
+    let mut vec_thread = Vec::new();
+    
+    for _i in 0..nbrequest {
+        let handle = thread::spawn(move || {
+            let mut stream = TcpStream::connect("127.0.0.1:7878").unwrap();
+
+            //write
+            let rand_u64: u64 = rand::thread_rng().gen();
+			let array:[u8;8]=transform_u64_to_array_of_u8(rand_u64);
+
+            //let request: String = rand_u64.to_string();
+            //stream.write(request.as_bytes()).unwrap();
+			stream.write(&array).unwrap();
+
+            //read
+            let mut buffer:[u8;8] = [0;8];
+            stream.read(&mut buffer).unwrap();
+			let plain= u64::from_be_bytes(buffer);
+            //println!("Received : {}", String::from_utf8_lossy(&buffer));
+			println!("Received : {}",plain);
+        });
+        vec_thread.push(handle);
+    }
+
+    for t in vec_thread {
+        t.join().unwrap();
+    }
+	let end = PreciseTime::now();
+	println!("{} seconds for executing several client threads", start.to(end));
---
+++ Project_PC2RServer/sandbox/web_server/src/bin/main.rs
@@ -0,0 +1,37 @@
+use std::io::prelude::*;
+use std::net::TcpStream;
+use std::net::TcpListener;
+use web_server::ThreadPool;
+
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
+	//let nbconnection = Arc::new(Mutex::new(0));
+	//let countdown:u8 = 20;
+	//let server_tickrate:u8=10;
+
+    for stream in listener.incoming() {
+		//let nbconnection = Arc::clone(&nbconnection);
+        let stream = stream.unwrap();
+        pool.execute(|| {
+            handle_connection(stream);
+        });
+    }
+}
+
+
+fn handle_connection(mut stream: TcpStream) {
+	//let mut nbplayer = nbconnection.lock().unwrap();
+	//*nbplayer++;
+	//if(*nbplayer==1){
+	//}
+
+	let mut buffer = String::new();
+    stream.read_to_string(&mut buffer)?;
+    //stream.read(&mut buffer).unwrap();
+	//let mut received= String::from_utf8_lossy(&buffer[..]).to_ascii_lowercase();
+	println!("Request: {}",buffer );
+	//Nouvelle connexion d’un client nomme ’user’	}
+    
+
+}
