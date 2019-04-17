f7d070a7c87a796827f18b9f71ac7f6bbb8b28a2
El Amraoui Adel <Elamraoui.Adel@hotmail.com>
2019-04-18 00:13:28 +0200

    Server with countdown and phases

--- Project_PC2RServer/sandbox/web_server/src/bin/main.rs
+++ Project_PC2RServer/sandbox/web_server/src/bin/main.rs
@@ -18,12 +18,11 @@ fn main() {
 	//init
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
     let pool = ThreadPool::new(4);
-	let actual_phase=Phase::attente;
 	println!("Server is running...");
-
 	//variable en acces partage
 	let mut pl:Vec<String> = Vec::new();
 	let mut Countdown_start= Instant::now();
+	let actual_phase=Phase::attente;
 	//let mut s:Vec<String> = Vec::new();
 	//let mut coords:Vec<String> = Vec::new();
 	
@@ -127,66 +126,3 @@ fn handle_connection(mut stream: TcpStream,p: Arc<Mutex<Vec<String>>>,c:Arc<Mute
     
 }
 
-
-/*
-extern crate timer;
-extern crate chrono;
-use std::sync::mpsc::channel;
-use std::io::prelude::*;
-use std::net::TcpStream;
-use std::net::TcpListener;
-use web_server::ThreadPool;
-use std::sync::{Arc, Mutex};
-
-fn main() {
-    let listener = TcpListener::bind("127.0.0.1:7870").unwrap();
-    let pool = ThreadPool::new(4);
-	println!("Server is running...");
-	//let mut players:Vec<&str> = Vec::new();
-	let mut nbplayers =0;
-	//let players: Arc<Mutex<Vec<&str>>> = Arc::new(Mutex::new(players));
-
-
-    for stream in listener.incoming() {
-        let stream = stream.unwrap();
-		nbplayers=nbplayers+1;
-		//countdown
-		if nbplayers ==1 {
-			println!("The first player is trying to connect, countdown start(10s)...");
-			let timer = timer::Timer::new();
-			let (tx, rx) = channel();
-			let _guard = timer.schedule_with_delay(chrono::Duration::seconds(10), move || {let _ignored = tx.send(());});
-			rx.recv().unwrap();
-			println!("10 seconds after first connection");
-		}
-
-        pool.execute(|| {
-            handle_connection(stream);
-        });
-    }
-}
-
-fn handle_connection(mut stream: TcpStream) {
-    let mut buffer = [0;128];
-    stream.read(&mut buffer).unwrap();
-	let s=String::from_utf8_lossy(&buffer[..]);
-
-	let split =s.split("/");
-	let vec: Vec<&str> = split.collect();
-	println!("Nouvelle connexion d'un client nomme {}",vec[1]);
-
-	//let mut players = players.lock().unwrap();
-	/*
-	if !players.contains(&vec[1]) {
-		players.push(&vec[1]);
-		println!("Nouvelle connexion d'un client nomme {}",vec[1]);
-	}
-	else{
-		println!("Connection denied , user {} already exists",vec[1]);
-	}
-	*/
-	//std::mem::drop(players);
-    stream.write(&buffer).unwrap();
-    stream.flush().unwrap();
-}
-*/
