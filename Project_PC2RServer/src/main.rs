c5d5876f8d533a8c998d3af8adb69c0f44339c8d
El Amraoui Adel <Elamraoui.Adel@hotmail.com>
2019-05-04 16:22:35 +0200

    restructuring the rust project

--- Project_PC2RServer/src/main.rs
+++ Project_PC2RServer/src/main.rs
@@ -1,139 +1,245 @@
+extern crate num_cpus;
+extern crate rand;
+
 use std::io::prelude::*;
 use std::net::TcpStream;
 use std::net::TcpListener;
-use web_server::{ThreadPool, Cell};
-use std::sync::{Arc, Mutex, mpsc};
-use rand::prelude::*;
-use std::env;
-use std::{thread, time};
+use web_server::ThreadPool;
+use std::sync::{Arc, Mutex};
+use std::time::Instant;
+use std::sync::atomic::{
+    AtomicBool,
+    Ordering::*,
+};
+use rand::Rng;
+//use std::env;
 
+//for print
+#[derive(Debug)]
+#[derive(Clone)]
+#[derive(PartialEq)]
+enum Phase {
+    attente,
+    jeu,
+}
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let size: usize = args[1].parse().unwrap();
+	//init
+	let num = num_cpus::get();
+	let pool = ThreadPool::new(num);
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+	
+   
+	println!("Server is running on {} cores...",num);
+	//variables en acces partage
+	let mut pl:Vec<String> = Vec::new();
+	let mut countdown_start= Instant::now();
+	let actual_phase=Phase::attente;
+	let debut = AtomicBool::new(true);
+	let mut s:Vec<String> = Vec::new();
+	let mut coords:Vec<String> = Vec::new();
+	
+	//objectif initiale
+	let mut rng = rand::thread_rng();
+	let x=rng.gen_range(0.000,800.000);
+	let y=rng.gen_range(0.000,800.000);
+	let objectif=format!("X{:.6}Y{:.6}",x.to_string(),y.to_string());
 
-    let vec: Vec<Cell> = vec![Cell { plain: 0, key: 0 }; size];
-    let vec: Arc<Mutex<Vec<Cell>>> = Arc::new(Mutex::new(vec));
-
-    let counter = Arc::new(Mutex::new(0));
-    let counter_write = Arc::new(Mutex::new(0));
-    let pool = ThreadPool::new(size);
-    let (sender, receiver) = mpsc::channel();
-    let (s, r) = mpsc::channel();
-    let r = Arc::new(Mutex::new(r));
-    let s = Arc::new(Mutex::new(s.clone()));
+	
+	let players = Arc::new(Mutex::new(pl));
+	let cs = Arc::new(Mutex::new(countdown_start));
+	let phase = Arc::new(Mutex::new(actual_phase));
+	let deb=Arc::new(debut);
+	let scores = Arc::new(Mutex::new(s));
+	let coords = Arc::new(Mutex::new(coords));
+	let obj=Arc::new(Mutex::new(objectif));
 
 
-    let (sd, rd) = mpsc::channel();
-    let rd = Arc::new(Mutex::new(rd));
-    let sd = Arc::new(Mutex::new(sd.clone()));
-    let counter_wait = Arc::new(Mutex::new(0));
-
-
-    let receiver = Arc::new(Mutex::new(receiver));
-    let sender = Arc::new(Mutex::new(sender.clone()));
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        let sender = Arc::clone(&sender);
-        let receiver = Arc::clone(&receiver);
-        let r = Arc::clone(&r);
-        let s = Arc::clone(&s);
+		let p = Arc::clone(&players);
+		let c=Arc::clone(&cs);
+		let ph=Arc::clone(&phase);
+		let d =deb.clone();
+		let sc=Arc::clone(&scores);
+		let cor=Arc::clone(&coords);
+		let o=Arc::clone(&obj);
 
-        let rd = Arc::clone(&rd);
-        let sd = Arc::clone(&sd);
-        let counter_wait = Arc::clone(&counter_wait);
-
-
-        let counter = Arc::clone(&counter);
-        let buffer = Arc::clone(&vec);
-        let counter_write = Arc::clone(&counter_write);
-
-        pool.execute(move || {
-            handle_connection(stream, counter, buffer, sender, receiver, size, counter_write, s, r, sd, rd, counter_wait);
+        pool.execute(|| {
+            handle_connection(stream,p,c,ph,d,sc,cor,o);
         });
+
     }
 }
 
-
-pub fn handle_connection(mut stream: TcpStream, counter: Arc<Mutex<i32>>, buf: Arc<Mutex<Vec<Cell>>>,
-                         sen: Arc<Mutex<mpsc::Sender<()>>>, rec: Arc<Mutex<mpsc::Receiver<()>>>,
-                         size: usize, counter_write: Arc<Mutex<i32>>, s: Arc<Mutex<mpsc::Sender<()>>>,
-                         r: Arc<Mutex<mpsc::Receiver<()>>>, sd: Arc<Mutex<mpsc::Sender<()>>>,
-                         rd: Arc<Mutex<mpsc::Receiver<()>>>, counter_wait: Arc<Mutex<i32>>) {
-    let mut buffer = [0; 8];
+fn handle_connection(mut stream: TcpStream,p: Arc<Mutex<Vec<String>>>,c:Arc<Mutex<Instant>>,
+					ph:Arc<Mutex<Phase>>,d:Arc<AtomicBool>,sc: Arc<Mutex<Vec<String>>>,cor: Arc<Mutex<Vec<String>>>,
+					o:Arc<Mutex<String>>) {
+	
+	loop{
+	let mut buffer = [0;128];
     stream.read(&mut buffer).unwrap();
-    let plain = u64::from_be_bytes(buffer);
-    let key = rand::thread_rng().gen();
+	let s=String::from_utf8_lossy(&buffer[..]);
 
+	let split =s.split("/");
+	let vec: Vec<&str> = split.collect();
+	let name= vec[1];
+	let r= vec[0];
 
-    let mut counter_thread = counter.lock().unwrap();
-    if *counter_thread == -1 {
-        let _received = r.lock().unwrap().recv().unwrap();
-        *counter_thread = 0;
-    }
-    let index = *counter_thread;
-    *counter_thread += 1;
-    std::mem::drop(counter_thread);
-    assert!(index >= 0 && index <= size as i32);
+	let mut players = p.lock().unwrap();
+	let mut scores = sc.lock().unwrap();
+	let mut coords = cor.lock().unwrap();
+	let mut obj=o.lock().unwrap();
 
-    let mut buff = buf.lock().unwrap();
-    buff[index as usize].key = key;
-    buff[index as usize].plain = plain;
-    std::mem::drop(buff);
+	let name=String::from(vec[1]);
+	let countdown = c.lock().unwrap();
+	let phase=ph.lock().unwrap();
 
+	//pour pouvoir affecter de nouvelles valeurs a la variable partagee
+	let mut count=countdown.clone();
+	let mut pha=phase.clone();
+	let mut objectif=obj.clone();
 
-    if index != (size as i32 - 1) {
-        let mut c_wait = counter_wait.lock().unwrap();
-        *c_wait += 1;
-        if *c_wait == (size as i32) - 1 {
-            sd.lock().unwrap().send(()).unwrap();
-            *c_wait = 0;
-        }
-        std::mem::drop(c_wait);
-        let _received = rec.lock().unwrap().recv().unwrap();
-        let buff = buf.lock().unwrap();
-        let result = buff[index as usize].plain;
-        std::mem::drop(buff);
-        assert!(result == key ^ plain);
-        stream.write(&u64_to_array_of_u8(result)).unwrap();
-        let mut c_write = counter_write.lock().unwrap();
-        *c_write += 1;
-        if *c_write == (size as i32) - 1 {
-            s.lock().unwrap().send(()).unwrap();
-            *c_write = 0;
-        }
-    } else {
-        rd.lock().unwrap().recv().unwrap();
-        let mut buff = buf.lock().unwrap();
-        for i in 0..(size) {
-            buff[i].plain = buff[i].plain ^ buff[i].key;
-            thread::sleep(time::Duration::from_millis(1));
-        }
-        assert!(buff[size - 1].plain == key ^ plain);
-        stream.write(&u64_to_array_of_u8(buff[size - 1].plain)).unwrap();
-        std::mem::drop(buff);
+	let mut modifcoords=coords.clone();
+	
+	match r{
+	"CONNECT"=>{
+		if d.load(Acquire){
+			count=Instant::now();
+			pha=Phase::attente;
+			d.store(false, Release);
+		}
+		if players.contains(&name){
+			println!("=======================================================");
+			println!("Nouvelle connexion d'un client nomme {}",name);
+			println!("Refus de connection: l'utilisateur {} existe deja",name);
+			println!("=======================================================");
+			let req = format!("{}","DENIED/");
+			stream.write(req.as_bytes()).unwrap();
+			stream.flush().unwrap();
+			println!("{}", req);
+			println!("Contents of players:");
+			for x in players.iter() {
+				println!("> {}", x);
+			}
+		}
+		else{
+			println!("=======================================================");
+			println!("Nouvelle connexion d'un client nomme {}",name);
+			println!("Connection etablie avec l'utilisateur {}",name);
+			println!("=======================================================");
+			let duration=count.elapsed().as_secs();
+			
+			//coordonees initiales
+			let mut rng = rand::thread_rng();
+			let x=rng.gen_range(0.000,800.000);
+			let y=rng.gen_range(0.000,800.000);
+			let coord=format!("{}:X{:.6}Y{:.6}",name.clone(),x.to_string(),y.to_string());
+			coords.push(coord);
+			let jcoords= coords.join("|");
+			println!("\n");
+			println!("Contents of coords:");
+			println!("{}",jcoords);
+			println!("\n");
 
-        let mut counter_thread = counter.lock().unwrap();
-        *counter_thread = -1;
-        std::mem::drop(counter_thread);
+			if duration >=20{
+				println!("\n");
+				println!("Lancement d'une session:");
+			    println!("\n");
+				pha=Phase::jeu;
 
-        for _ in 0..(size - 1) {
-            sen.lock().unwrap().send(()).unwrap();
-        }
-    }
+				let session = format!("{}{}/{}","SESSION/",jcoords,objectif);
+				stream.write(session.as_bytes()).unwrap();
+				stream.flush().unwrap();
+				/*
+				for x in players.iter() {
+					println!("> {}", x);
+				}
+				*/
+				
+				
+			}
+
+			//players
+			players.push(name.clone());
+			//scores
+			let score_user=format!("{}{}{}",name.clone(),":",String::from("0"));
+			scores.push(score_user);
+			let jscores= scores.join("|");
+			println!("\n");
+			println!("Contents of scores:");
+			println!("{}",jscores);
+			println!("\n");
+
+			
+			
+			//envoi au client qui a emit la requete
+			let req_jeu = format!("{}{:?}/{}/{}/","WELCOME/",pha,jscores,objectif);
+			let req_attente = format!("{}{:?}/{}/","WELCOME/",pha,jscores);
+			
+			if pha==Phase::jeu {
+				stream.write(req_jeu.as_bytes()).unwrap();
+				stream.flush().unwrap();
+			}
+			else{
+				stream.write(req_attente.as_bytes()).unwrap();
+				stream.flush().unwrap();
+			}
+			
+			let new_player = format!("{}{}","NEWPLAYER/",name.clone());
+			stream.write(new_player.as_bytes()).unwrap();
+			stream.flush().unwrap();
+			
+		}
+	},
+	"EXIT"=>{
+		println!("le client nomme {} se deconnecte",name);
+		let index = players.iter().position(|x| *x == name).unwrap();
+		players.remove(index);
+		println!("Contents of players:");
+		for x in players.iter() {
+			println!("> {}", x);
+		}
+
+		let player_left = format!("{}{}/","PLAYERLEFT/",name.clone());
+		stream.write(player_left.as_bytes()).unwrap();
+		stream.flush().unwrap();
+		break;
+	},
+	"NEWPOS"=>{
+		//vec[1]= "riri:X0.000125Y-2.5"
+		let newcoord= vec[1];
+		let splitnew =newcoord.split(":");
+		let vec_coord: Vec<&str> = splitnew.collect();
+		for i in 0..coords.len() {
+			let sp=coords[i].split(":");
+			let vsp:Vec<&str>=sp.collect();
+			if vec_coord[0]==vsp[0] {
+				coords[i]=String::from(newcoord);
+			}
+		}
+
+		let jcoords2= coords.join("|");
+		let tick = format!("{}{}/","TICK/",jcoords2);
+		stream.write(tick.as_bytes()).unwrap();
+		stream.flush().unwrap();
+		
+		println!("=======================================================");
+		println!("Modification des coordonnes:");
+		println!("{}",jcoords2);
+		println!("=======================================================");
+
+	},
+	 _ => println!("unknown request {} ",r),
+	}
+	std::mem::drop(players);
+	std::mem::drop(scores);
+	std::mem::drop(coords);
+
+	std::mem::drop(countdown);
+	std::mem::drop(phase);
+    stream.write(&buffer).unwrap();
+	}
 }
 
-
-fn u64_to_array_of_u8(x: u64) -> [u8; 8] {
-    let b1: u8 = ((x >> 56) & 0xff) as u8;
-    let b2: u8 = ((x >> 48) & 0xff) as u8;
-    let b3: u8 = ((x >> 40) & 0xff) as u8;
-    let b4: u8 = ((x >> 32) & 0xff) as u8;
-    let b5: u8 = ((x >> 24) & 0xff) as u8;
-    let b6: u8 = ((x >> 16) & 0xff) as u8;
-    let b7: u8 = ((x >> 8) & 0xff) as u8;
-    let b8: u8 = (x & 0xff) as u8;
-    return [b1, b2, b3, b4, b5, b6, b7, b8];
-}
